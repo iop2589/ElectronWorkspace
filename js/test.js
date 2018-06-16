f6fafea2e2b8afa48ccb62eecd67e7ac7e0642bf
박성철 <35950314+iop2589@users.noreply.github.com>
2018-06-17 01:50:05 +0900

    alert js 추가

--- js/test.js
+++ js/test.js
@@ -1,5 +1,9 @@
 
 
 $("#test").click(function(){
-    alert("hi");
+    Ext.Msg.show({ 
+        title : 'alert'
+        , msg : 'hi'
+        , icon : Ext.Msg.INFO
+        , buttons : Ext.Msg.OK });
 });
