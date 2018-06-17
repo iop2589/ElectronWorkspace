38007e228807eb96da23b69a851c2c4f402e4b0b
박성철 <35950314+iop2589@users.noreply.github.com>
2018-06-18 02:17:15 +0900

    인스타그램 개발

--- js/test.js
+++ js/test.js
@@ -1,4 +1,19 @@
+var Instafeed = require("instafeed.js");
 
+$(function(){
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '1776199106',
+        sortBy: 'none',
+        limit: 20,
+        //template: '<webview src="{{link}}" plugins="on">{{like}}</webview>', 
+        template: '<a href="{{link}}" target="_blank"><img src="{{image}}" />내용 : {{caption}}, 좋아요 : {{likes}}</a> <br />', 
+        // {{link}} : 게시물 링크, {{image}} : 사진 url, {{caption}} : 게시물 텍스트
+        accessToken: '1776199106.c961016.6f08914f0af642b8b32d9605ac4c2fd0'    
+    });
+     
+    userFeed.run();
+});
 
 $("#test").click(function(){
     swal ( "test click" ,  "이쁜 경고창" ,  "success" ); // "warning", "error", "success" and "info".
